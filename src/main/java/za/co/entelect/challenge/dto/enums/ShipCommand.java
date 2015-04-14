6b1bfb6f56c02704bdd097f4c42fad95e16654c9
Hendrik Coetzee <hs.coetzee@gmail.com>
2015-04-14 21:00:33 +0200

    A cleaner, more civilized use of enums ;)

--- src/main/java/za/co/entelect/challenge/dto/enums/ShipCommand.java
+++ src/main/java/za/co/entelect/challenge/dto/enums/ShipCommand.java
@@ -2,27 +2,13 @@ package za.co.entelect.challenge.dto.enums;
 
 public enum ShipCommand {
 
-    NOTHING("Nothing", 0),
-    MOVE_LEFT("MoveLeft", 1),
-    MOVE_RIGHT("MoveRight", 2),
-    SHOOT("Shoot", 3),
-    BUILD_ALIEN_FACTORY("BuildAlienFactory", 4),
-    BUILD_MISSILE_CONTROLLER("BuildMissileController", 5),
-    BUILD_SHIELD("BuildShield", 6);
+	Nothing,
+	MoveLeft,
+	MoveRight,
+	Shoot,
+	BuildAlienFactory,
+	BuildMissileController,
+	BuildShield;
 
-    private ShipCommand(String moveDescription, int moveNumber) {
-        this.moveDescription = moveDescription;
-        this.moveNumber = moveNumber;
-    }
-
-    private String moveDescription;
-    private int moveNumber;
-
-    public String getMoveDescription() {
-        return moveDescription;
-    }
-
-    public int getMoveNumber() {
-        return moveNumber;
-    }
+   
 }
--- src/main/java/za/co/entelect/challenge/utils/BotHelper.java
+++ src/main/java/za/co/entelect/challenge/utils/BotHelper.java
@@ -5,15 +5,6 @@ import za.co.entelect.challenge.dto.enums.ShipCommand;
 public class BotHelper {
 
     public static String translateMove(int number) {
-        String latestMove = ShipCommand.NOTHING.getMoveDescription();
-
-        for (ShipCommand move : ShipCommand.values()) {
-            if (move.getMoveNumber() == number) {
-                latestMove = move.getMoveDescription();
-                break;
-            }
-        }
-
-        return latestMove;
+    	return ShipCommand.values()[number].toString();
     }
 }
